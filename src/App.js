1dea7c4f43f2d3472f89c98cbe4260095a4688bd
CemGOKCE <cemgokce9619@gmail.com>
2023-03-01 22:36:07 +0300

    Tailwind example project complited.

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Hero from "./component/Hero";
+import Navbar from "./component/Navbar";
+import Analytics from "./component/Analytics";
+import Newsletter from "./component/Newsletter";
+import Cards from "./component/Cards";
+import Footer from "./component/Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Analytics />
+      <Newsletter />
+      <Cards />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
